d2d6e9cf0898b8d343454f3165ac222264a49995
Victor Pasqual <vhbp2003@gmail.com>
2022-12-15 09:39:37 -0300

    Primeiro Commit

---
+++ node-test-api/src/test/routes/editorasRoutes.test.js
@@ -0,0 +1,54 @@
+import request from 'supertest';
+import app from '../../app.js';
+
+let server;
+beforeEach(() => {
+  const port = 3000;
+  server = app.listen(port);
+});
+afterEach(() => {
+  server.close();
+});
+
+describe('GET em /editoras', () => {
+  it('Deve retornar uma lista de editoras', async () => {
+    const resposta = await request(app)
+      .get('/editoras')
+      .set('Accept', 'application/json')
+      .expect('content-type', /json/)
+      .expect(200);
+
+    expect(resposta.body[0].email).toEqual('e@e.com');
+  });
+});
+
+let idResposta;
+
+describe('POST em /editoras', () => {
+  it('Deve adicionar uma nova editora', async () => {
+    const resposta = await request(app)
+      .post('/editoras')
+      .send({
+        nome: 'CDC',
+        cidade: 'São Paulo',
+        email: 's@s.com',
+      })
+      .expect(201);
+
+    idResposta = resposta.body.content.id;
+  });
+  it('Deve map adicionar nada ao passsar o body vazio', async () => {
+    await request(app)
+      .post('/editoras')
+      .send({})
+      .expect(400);
+  });
+});
+
+describe('DELETE em /editoras', () => {
+  it('Deletar o recurso adcionado', async () => {
+    await request(app)
+      .delete(`/editoras/${idResposta}`)
+      .expect(200);
+  });
+});
